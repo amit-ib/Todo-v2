6cdaa43c33869098f966075b360c5df190da6a6c
Amit Verma <amit.verma@infobeans.com>
2022-09-16 17:54:21 +0530

    Setup Redux

---
+++ src/redux/todoReducer.tsx
@@ -0,0 +1,18 @@
+import { TodoModal } from "../models";
+import { staticTodo } from "../mock-data/todo";
+export interface todoList {
+  tasks: TodoModal[];
+}
+const initialState = {
+  tasks: staticTodo,
+};
+
+//(previousState, action) => newState
+const todoReducer = (state: todoList = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
