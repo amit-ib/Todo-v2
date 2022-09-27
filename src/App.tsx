99e4d173f0e4bf777ce70e15a4a2eb5844071547
Amit Verma <amit.verma@infobeans.com>
2022-09-27 17:53:59 +0530

    Re-uploading APP.tsx get missed to be pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
 import TodoForm from "./components/todo/TodoForm";
 import TodoList from "./components/todo/TodoList";
-import Login from "./components/shared/Login";
+import Login from "./components/auth/Login";
 import { useDispatch, useSelector } from "react-redux";
-import { reducerSate } from "./store/todoReducer";
+import { statesModal } from "./store/todoReducer";
 import Button from "./components/shared/form/Button";
 import { loginTodoAction } from "./store";
 
 function App() {
-  const state = useSelector((state: reducerSate) => state);
+  const state = useSelector((state: statesModal) => state);
   const dispatch = useDispatch();
   var accessToken = window.localStorage.getItem("accessToken");
+  let formTitle = state.isLogedin ? "Todo List" : "Todo Login";
   const userName = window.localStorage.getItem("userName")
     ? window.localStorage.getItem("userName")
     : "";
@@ -33,17 +34,13 @@ function App() {
         {state.isLogedin && (
           <span>
             Welcome - {userName} |{" "}
-            <Button label="Logout" onClick={onLogoutSuccess} styles="link" />
+            <Button label="Logout" onClick={onLogoutSuccess} varient="link" />
           </span>
         )}
       </div>
       <div className="container">
         <div className="todo-container">
-          {state.isLogedin ? (
-            <h3 className="text-center">Todo List</h3>
-          ) : (
-            <h3 className="text-center">Login</h3>
-          )}
+          <h3 className="text-center">{formTitle}</h3>
 
           {state.isLogedin && (
             <div>
