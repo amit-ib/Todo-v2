a8fbd66f244e9f31d895a8361f4e90e2f92314a8
Amit Verma <amit.verma@infobeans.com>
2022-10-03 16:36:57 +0530

    TDAP055-Backend-Connect-Delete-Todos-And-Tost-Messages

--- src/components/shared/Confirm.jsx
+++ src/components/shared/Confirm.jsx
@@ -3,37 +3,33 @@ import { ConfimModel } from "../../models/confirm.models";
 import Button from "./form/Button";
 
 const BsModal = (props: ConfimModel) => {
+  const cancelButtonHandler = () => {
+    props.onHide();
+  };
+  const actionButtonHandler = () => {
+    props.buttonAction();
+  };
 
-    const cancelButtonHandler = () =>
-    {
-        props.onHide()
-    }
-    const actionButtonHandler = () =>
-    {
-        props.buttonAction()
-    }
-    
-    return (
-        <Modal show={props.show} onHide={cancelButtonHandler}>
-            <Modal.Header closeButton>
-                <Modal.Title>{props.title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{props.text}</Modal.Body>
-            <Modal.Footer>
-                <Button
-                    label="Close"
-                    styles="secondary"
-                    onClick={()=>cancelButtonHandler()}
-                   
-                />
-                <Button
-                    label={props.buttonLabel}
-                    styles="primary"
-                    onClick={() => actionButtonHandler()}
-                />
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={props.show} onHide={cancelButtonHandler}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{props.text}</Modal.Body>
+      <Modal.Footer>
+        <Button
+          label="Close"
+          varient="secondary"
+          onClick={() => cancelButtonHandler()}
+        />
+        <Button
+          label={props.buttonLabel}
+          varient="primary"
+          onClick={() => actionButtonHandler()}
+        />
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default BsModal;
